298211e0b881ccb1d12a44e98bbbd1fcfde643f4
ManojGowda2006 <manojgowdan807@gmail.com>
2025-02-08 11:03:42 +0530

    complete the task

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Manoj Gowda N</h1>
+      <p>Email : manojgowdan807@gmail.com</p>
+      <p>constact : 901XX XXXXX</p>
+    </>
+  )
+}
+
+export default App
